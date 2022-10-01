ab30e41e0538cf2e61d35fa236f5e4b96ccba299
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-01 15:36:25 -0600

    Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,35 +8,10 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
+    <?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/post1">My First Post</a></h1>
-        <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, 
-            so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
-            weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, 
-            when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
-            But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances 
-            accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures 
-            pains to avoid worse pains."</p>
+        <?= $post; ?>
     </article>
-    <article>
-        <h1><a href="/posts/post2">My Second Post</a></h1>
-        <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, 
-            so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
-            weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, 
-            when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
-            But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances 
-            accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures 
-            pains to avoid worse pains."</p>
-    </article>
-    <article>
-        <h1><a href="/posts/post3">My Third Post</a></h1>
-        <p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, 
-            so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through 
-            weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, 
-            when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. 
-            But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances 
-            accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures 
-            pains to avoid worse pains."</p>
-    </article>
+    <?php endforeach; ?>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,17 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('posts');
+    $posts = Post::all();
+    return view('posts', [
+        'posts' => $posts,
+    ]);
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    if(!file_exists($path)){
-        //dd('file does not exist'); //or ddd
-        //abort(404);
-        return redirect('/');
-    }
-    $post = cache()->remember("posts.{$slug}", 3600, function () use ($path) { //or now()->addMinutes(60)
-        //var_dump('file_get_contents');
-        return file_get_contents($path);
-    });
-    
+    $post = Post::find($slug);
     return view('post', [
         'post' => $post,
     ]);
