ec35fab75f719ac4b5695605923e8d1719a5f5aa
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-02 16:38:38 -0600

    Make a Post Model and Migration

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/posts/{post}', function ($slug) {
-    $post = Post::findOrFail($slug);
+Route::get('/posts/{post}', function ($id) {
+    $post = Post::findOrFail($id);
     return view('post', [
         'post' => $post,
     ]);
