57c55d9bd68b573bb302485b9749e2ce61a61db2
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-02 19:20:17 -0600

    Your First Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,28 +2,35 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
-    //protected $guarded = ['id']; //All are fillable except id
+    protected $guarded = []; //If we want to make all the fields mass assignable
 
-    //protected $guarded = []; //If we want to make all the fields mass assignable
+    //protected $guarded = ['id']; //All are fillable except id
 
     //protected $guarded = ['*']; //If we want to block all the fields from being mass-assigned
 
-    protected $fillable = [
-        'title',
-        'excerpt',
-        'body',
-    ];
+    // protected $fillable = [ //All these are fillable
+    //     'title',
+    //     'excerpt',
+    //     'body',
+    // ];
 
     //Alternativa para buscar por slug
     // public function getRouteKeyName()
     // {
     //     return 'slug';
     // }
+
+    public function category() 
+    {
+        //hasOne, hasMany, belongsTo, belongsToMany
+        return $this->belongsTo(Category::class);
+    }
 }
