0346e6ec604286b0e2b75fb02697f9c88ed295c7
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-03 14:04:06 -0600

    Database Seeding Saves Time

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts() 
+    {
+        //hasOne, hasMany, belongsTo, belongsToMany
+        return $this->hasMany(Post::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work',
+        ]);
+        $hobbies = Category::create([
+            'name' => 'Hobbies',
+            'slug' => 'hobbies',
+        ]);
+
+        Post::create([
+            'title' => 'My Family Post',
+            'excerpt' => 'Excerpt for my post',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'slug' => 'my-family-post',
+            'category_id' => $personal->id,
+            'user_id' => $user->id,
+        ]);
+        Post::create([
+            'title' => 'My Work Post',
+            'excerpt' => 'Excerpt for my post',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'slug' => 'my-work-post',
+            'category_id' => $work->id,
+            'user_id' => $user->id,
+        ]);
+        Post::create([
+            'title' => 'My Hobby Post',
+            'excerpt' => 'Excerpt for my post',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'slug' => 'my-hobby-post',
+            'category_id' => $hobbies->id,
+            'user_id' => $user->id,
+        ]);
     }
 }
