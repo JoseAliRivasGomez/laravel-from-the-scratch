6ba5321e53b32046b3e813e1ca9455b0089983f6
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-02 19:31:27 -0600

    Show All Posts Associated With a Category

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,9 @@ Route::get('/posts/{post:slug}', function (Post $post) { //Post::where('slug', $
         'post' => $post,
     ]);
 }); //where, or whereAlpha, whereNumber, whereAlphaNumeric
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts,
+    ]);
+});
