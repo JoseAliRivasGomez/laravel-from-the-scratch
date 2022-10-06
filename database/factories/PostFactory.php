a1f05b2b2986d5c55254160be639406acc4c2095
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-06 16:31:46 -0600

    Quick Tweaks and Clean-Up

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,9 +19,9 @@ class PostFactory extends Factory
             'user_id' => User::factory(),
             'category_id' => Category::factory(),
             'title' => $this->faker->sentence(),
-            'excerpt' => $this->faker->sentence(),
             'slug' => $this->faker->slug(),
-            'body' => $this->faker->paragraph(),
+            'excerpt' => '<p>' . implode('</p><p>', $this->faker->paragraphs(2)) . '</p>',
+            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(6)) . '</p>',
         ];
     }
 }
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -6,7 +6,7 @@
         {{$trigger}}
     </div>
 
-    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50" style="display: none">
+    <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50 overflow-auto max-h-64" style="display: none">
         {{$slot}}
     </div>
 </div>
