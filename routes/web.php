82e09a9d6f68b4b15e8da45bce62dfba0831a768
José Alí Rivas Gómez <100250771+JoseAliRivasGomez@users.noreply.github.com>
2022-10-06 17:32:20 -0600

    Advanced Eloquent Query Constraints

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('post'); //where, or whereAlpha, whereNumber, whereAlphaNumeric
 
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        //'posts' => $category->posts->load(['category', 'author']),
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
-
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         //'posts' => $author->posts->load(['category', 'author']),
